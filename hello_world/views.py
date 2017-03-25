6be6cb1305df98e4bc4ce69755005b7bded4e488
root <you@example.com>
2017-03-25 11:48:28 +0100

    Zmiana imienai

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Wojtek"
+moje_imie = "Diana"
 msg = "Hello World!"
 
 @app.route('/')
